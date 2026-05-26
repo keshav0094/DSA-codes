179e70e16bad7fc0808f523b4ac6cf95a4453971
keshav0094 <keshavmurari0094@gmail.com>
2026-05-26 19:27:44 +0530

    Add aggressive cows problem implementation

---
+++ aggresive_cows.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool isValid(vector<int> stalls, int k, int mid){
+    int cow = 1;
+    int lastCowPos = stalls[0];
+
+    for (int i=1; i<stalls.size(); i++){
+        if (stalls[i]-lastCowPos >= mid){
+            lastCowPos = stalls[i];
+            cow++ ;
+        }
+        if (cow==k){
+            return true;
+        }
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int>& stalls, int k){
+    sort(stalls.begin() , stalls.end());
+
+    int mini = INT_MAX;
+    int maxi = INT_MIN;
+
+    for(int val:stalls){
+        mini = min(mini,val);
+        maxi = max(maxi,val);
+    }
+
+    int st = 1;
+    int end = maxi-mini;
+    int ans = -1;
+
+    while(st<=end){
+        int mid = st + (end-st)/2;
+
+        if (isValid(stalls, k, mid)){
+            ans = mid;
+            st = mid+1;
+        } else{
+            end = mid-1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> stalls = {1, 2, 4, 8, 9};
+    int k = 3;
+
+    cout << aggressiveCows(stalls, k);
+    return 0;
+}
